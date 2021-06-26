8d2ef4781b1d93dff9dcd10e4016dc3e351af722
spences10 <spences10apps@gmail.com>
2021-06-26 20:49:19 +0100

    feat: :sparkles: add header component

---
+++ src/lib/header.svelte
@@ -0,0 +1,3 @@
+<header class="mb-8 mt-4">
+  <h1 class="text-4xl">SvelteKit GraphCMS Todos</h1>
+</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Header from '$lib/header.svelte'
   import '../app.postcss'
 </script>
 
 <main class="container max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+  <Header />
   <slot />
 </main>
