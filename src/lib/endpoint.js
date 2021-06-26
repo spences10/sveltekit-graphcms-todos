c5121cfbc76453f3ed4dabfb639b4cfb422ba3d5
spences10 <spences10apps@gmail.com>
2021-06-26 13:19:38 +0100

    feat: :sparkles: add GraphQL client

---
+++ src/lib/endpoint.js
@@ -0,0 +1,2 @@
+export const API_URL = process.env['GRAPHCMS_URL']
+export const API_TOKEN = process.env['GRAPHCMS_MUTATION_TOKEN']
---
+++ src/lib/graphql-client.js
@@ -0,0 +1,8 @@
+import { GraphQLClient } from 'graphql-request'
+import { API_TOKEN, API_URL } from './endpoint'
+
+export const graphCmsClient = new GraphQLClient(API_URL, {
+  headers: {
+    authorization: `Bearer ${API_TOKEN}`,
+  },
+})
