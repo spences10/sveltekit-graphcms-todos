93e8339671b98a29ec93aa6d3da3d13bd39cbafe
spences10 <spences10apps@gmail.com>
2021-07-07 18:29:16 +0100

    feat: :sparkles: add simple OAuth

--- src/lib/endpoint.js
+++ src/lib/endpoint.js
@@ -1,2 +1,6 @@
+// GraphCMS secrets
 export const API_URL = process.env['GRAPHCMS_URL']
 export const API_TOKEN = process.env['GRAPHCMS_MUTATION_TOKEN']
+// Authentication secrets
+export const CLIENT_ID = process.env['CLIENT_ID']
+export const CLIENT_SECRET = process.env['CLIENT_SECRET']
---
+++ src/routes/auth.js
@@ -0,0 +1,45 @@
+import { CLIENT_ID, CLIENT_SECRET } from '$lib/endpoint'
+
+const tokenUrl = 'https://github.com/login/oauth/access_token'
+const userUrl = 'https://api.github.com/user'
+const clientId = CLIENT_ID
+const secret = CLIENT_SECRET
+
+export async function get(req) {
+  const code = req.query.get('code')
+
+  const token = await getToken(code)
+  const user = await getUser(token)
+  req.locals.user = user.login
+
+  return {
+    status: 302,
+    headers: { location: '/' },
+  }
+}
+
+function getToken(code) {
+  return fetch(tokenUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      client_id: clientId,
+      client_secret: secret,
+      code,
+    }),
+  })
+    .then(response => response.json())
+    .then(response => response.access_token)
+}
+
+function getUser(token) {
+  return fetch(userUrl, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(response => response.json())
+}
